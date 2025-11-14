6e577da7fee6d9e0b5713fe5a7b448bf9c9a2287
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2025-11-14 15:49:25 -0300

    atualização vercel

--- prisma.js
+++ prisma.js
@@ -1,12 +1,13 @@
+// prisma.js
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+// O padrão global garante que o PrismaClient seja instanciado apenas uma vez,
+// mesmo que o container serverless seja "reutilizado" pela Vercel.
+// Isso evita erros de conexão e o erro "FUNCTION_INVOCATION_FAILED".
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) global.prisma = new PrismaClient();
-    prisma = global.prisma;
+if (process.env.NODE_ENV !== "production") {
+    global.prisma = prisma;
 }
-// teste
+
 export { prisma };
