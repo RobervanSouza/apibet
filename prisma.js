aab6f662a3d8383197046cafc1513b525207a8d2
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2025-11-14 16:45:53 -0300

    prisma

--- prisma.js
+++ prisma.js
@@ -1,11 +1,7 @@
-// prisma.js
 import { PrismaClient } from "@prisma/client";
 
-// Padrão de instância única para evitar erros em ambientes serverless
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-    global.prisma = prisma;
-}
+// Cria a instância do PrismaClient.
+// Em ambientes serverless, o provedor se conecta sob demanda.
+const prisma = new PrismaClient();
 
 export { prisma };
